70ff6b108c3587ea289202fae91976fe735ace73
Sergey <piolersa85@mail.ru>
2021-09-28 16:17:27 +0200

    Initial Commit

---
+++ CollectionCustom Cell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionCustom Cell
+//
+//  Created by Sergey on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
